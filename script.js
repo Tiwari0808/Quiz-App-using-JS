a58f22b22350ce747b990e04a14fd78bb920bb70
Divyanshu <divyanshutiwari173@gmail.com>
2024-08-25 17:54:42 +0530

    Final commit

--- script.js
+++ script.js
@@ -50,29 +50,77 @@ let score = 0;
 function startQuiz() {
     currQustIndex = 0;
     score = 0;
-    nextBtn.innerHTML= "Next";
+    nextBtn.innerHTML = "Next";
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
     resetState();
     let currentQuestion = questions[currQustIndex];
-    let questionNo = currQustIndex +1;
+    let questionNo = currQustIndex + 1;
     questionBtn.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    currentQuestion.answers.forEach(answer =>{
+    currentQuestion.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerHTML = answer.text;
         button.classList.add('btn');
         answerBtn.appendChild(button);
+
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
     });
 }
 
-function resetState(){
+resetState = () => {
     nextBtn.style.display = 'none';
-    while(answerBtn.firstChild){
+    while (answerBtn.firstChild) {
         answerBtn.removeChild(answerBtn.firstChild);
     }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct;
+    if (isCorrect === "true") {
+        selectedBtn.classList.add('correct');
+        score++;
+    } else {
+        selectedBtn.classList.add('incorrect');
+    }
+    Array.from(answerBtn.children).forEach(button => {
+        if (button.dataset.correct === 'true') {
+            button.classList.add('correct');
+        }
+        button.disabled = true;
+    });
+    nextBtn.style.display = 'block';
+}
+
+nextBtn.addEventListener('click', () => {
+  if(currQustIndex < questions.length){
+    handleNextBtn();
+  }else{
+    startQuiz();
+  }
+})
+
+
+handleNextBtn = () =>{
+    currQustIndex++;
+    if(currQustIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+showScore = () =>{
+    resetState();    
+    questionBtn.innerHTML = `You scored ${score} out of ${questions.length}`;
+    nextBtn.innerHTML = 'Replay'; 
+    nextBtn.style.display = 'block';
 }
 
 startQuiz();
